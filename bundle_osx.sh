8755ee97a5d43b30beef44fc200bdb678aded13e
Pierre Ferran <pierr3@users.noreply.github.com>
2022-02-02 13:13:30 +0100

    Added resources and OSX build script

---
+++ bundle_osx.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+#
+# Creating the macOs bundle
+#
+
+#
+# Copying all files
+#
+
+mkdir -p build/VectorAudio.app/Contents/{MacOS,Resources}
+cp resources/*.wav build/VectorAudio.app/Contents/Resources
+cp resources/VectorAudio.icns build/VectorAudio.app/Contents/Resources
+cp build/libafv.dylib build/VectorAudio.app/Contents/MacOS
+
+chmod +x build/afv_unix
+cp build/afv_unix build/VectorAudio.app/Contents/MacOS
+
+cat > build/VectorAudio.app/Contents/Info.plist << EOF
+{
+	CFBundleName = "vectoraudio";
+	CFBundleDisplayName = "Vector Audio";
+	CFBundleIndentifier = "org.vector.audio";
+	CFBundleVersion = "1";
+	CFBundleShortVersionString = "1";
+	CFBundleInfoDictionaryVersion = "6.0";
+	CFBundlePackageType = "APPL";
+	CFBundleExecutable = "afv_unix";
+	CFBundleIconFile = "VectorAudio.icns";
+    NSHighResolutionCapable = "false";
+}
+EOF
