42bae589ef4b0ff548df915a6b4e9dabe2505af1
Pierre Ferran <pierr3@users.noreply.github.com>
2023-07-18 02:23:16 -0700

    Update bundle_linux.sh

--- bundle_linux.sh
+++ bundle_linux.sh
@@ -6,28 +6,28 @@
 
 python3 collect_licenses.py
 
-mkdir build/VectorAudio.AppDir/
-cp -R resources/VectorAudio.AppDir/* build/VectorAudio.AppDir/
-cp resources/*.wav build/VectorAudio.AppDir/usr/share/vectoraudio/
-cp resources/*.ttf build/VectorAudio.AppDir/usr/share/vectoraudio/
-cp resources/LICENSE.txt build/VectorAudio.AppDir/usr/share/vectoraudio/
-cp resources/airports.json build/VectorAudio.AppDir/usr/share/vectoraudio/
-cp resources/icon_mac.png build/VectorAudio.AppDir/vectoraudio.png
-cp resources/icon_mac.png build/VectorAudio.AppDir/usr/share/vectoraudio/
+cp -R ./resources/VectorAudio.AppDir/ ./build/VectorAudio.AppDir/
+cp ./resources/*.wav ./build/VectorAudio.AppDir/usr/share/vectoraudio/
+cp ./resources/*.ttf ./build/VectorAudio.AppDir/usr/share/vectoraudio/
+cp ./resources/LICENSE.txt ./build/VectorAudio.AppDir/usr/share/vectoraudio/
+cp ./resources/airports.json ./build/VectorAudio.AppDir/usr/share/vectoraudio/
+cp ./resources/icon_mac.png ./build/VectorAudio.AppDir/vectoraudio.png
+cp ./resources/icon_mac.png ./build/VectorAudio.AppDir/usr/share/vectoraudio/
 
-cp build/extern/afv-native/afv_native.so build/VectorAudio.AppDir/usr/lib
-chmod +x build/VectorAudio.AppDir/usr/lib/afv_native.so
+cp ./build/extern/afv-native/afv_native.so ./build/VectorAudio.AppDir/usr/lib
+chmod +x ./build/VectorAudio.AppDir/usr/lib/afv_native.so
 
-cp build/vector_audio build/VectorAudio.AppDir/usr/bin
-chmod +x build/VectorAudio.AppDir/usr/bin/vector_audio
-chmod 755 build/VectorAudio.AppDir/usr/bin/vector_audio
+cp ./build/vector_audio ./build/VectorAudio.AppDir/usr/bin
+chmod +x ./build/VectorAudio.AppDir/usr/bin/vector_audio
+chmod 755 ./build/VectorAudio.AppDir/usr/bin/vector_audio
+chmod +x ./build/VectorAudio.AppDir/vectoraudio.desktop
 
 version=`cat VERSION`
-sed -i "s/VAVER/$version/" build/VectorAudio.AppDir/vectoraudio.desktop
+sed -i "s/VAVER/$version/" ./build/VectorAudio.AppDir/vectoraudio.desktop
 
-wget -O appimage-builder-x86_64.AppImage https://github.com/AppImageCrafters/appimage-builder/releases/download/v1.1.0/appimage-builder-1.1.0-x86_64.AppImage
-chmod +x appimage-builder-x86_64.AppImage
+wget -O appimagetool-x86_64.AppImage https://github.com/AppImage/appimagetool/releases/download/latest/appimagetool-x86_64.AppImage
+chmod +x appimagetool-x86_64.AppImage
 
-./appimage-builder-x86_64.AppImage --appdir build/VectorAudio.AppDir
+./appimagetool-x86_64.AppImage ./build/VectorAudio.AppDir
 
-mv build/VectorAudio.AppImage build/VectorAudio-x64.AppImage
+mv ./build/VectorAudio.AppImage ./build/VectorAudio-x64.AppImage
